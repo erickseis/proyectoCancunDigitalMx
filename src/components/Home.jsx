9a54cf3b9665db9d9baaa15cff0e4d04fbcd7572
erickseis <erickseis@gmail.com>
2022-11-03 17:04:23 -0300

    landing page firts

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,29 +4,53 @@ import img2 from '../assets/images/disenadores-web-barcelona.gif';
 
 const Home = () => {
   return (
-    <div>
-      <div className='containerBackgroundImage'>
-        <img className='backgroundImage' src={img2} alt='' />
+    <>
+      <div className='container-general'>
+        <div className='containerBackgroundImage'>
+          <img className='backgroundImage' src={img2} alt='' />
+        </div>
+        <div className='container'>
+          <div className='wave'></div>
+          <div className='wave2'></div>
+          <div className='wave3'></div>
+        </div>
+        <div className='header'>
+          <div className='title'>
+            <h1>CANCÚN</h1>
+            <br />
+            <h2>DIGITAL</h2>
+          </div>
+          <br />
+          <div className='service-about'>
+            <button className='btn btn-service'>
+              <a href=''>Servicios</a>
+            </button>
+            <button className='btn btn-about'>
+              <a href='#aboutUs'>Nosotros</a>
+            </button>
+          </div>
+        </div>
       </div>
-      <header className='container'>
-        <div className='wave'></div>
-        <div className='wave2'></div>
-        <div className='wave3'></div>
-      </header>
-      <h1>esta es mi ruta HOME</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab a,
-        explicabo corrupti repudiandae atque consectetur reprehenderit ad
-        molestias nobis nisi dolorum voluptates enim aperiam animi quos placeat
-        adipisci perspiciatis!ç
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab a,
-        explicabo corrupti repudiandae atque consectetur reprehenderit ad
-        molestias nobis nisi dolorum voluptates enim aperiam animi quos placeat
-        adipisci perspiciatis!ç
-      </p>
-    </div>
+      <section>
+        <article className='description-about'>
+          <h1 id='aboutUs'>NOSOTROS</h1>
+          <br />
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab a,
+            explicabo corrupti repudiandae atque consectetur reprehenderit ad
+            molestias nobis nisi dolorum voluptates enim aperiam animi quos
+            placeat adipisci perspiciatis!ç
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ab a,
+            explicabo corrupti repudiandae atque consectetur reprehenderit ad
+            molestias nobis nisi dolorum voluptates enim aperiam animi quos
+            placeat adipisci perspiciatis!ç
+          </p>
+        </article>
+      </section>
+    </>
   );
 };
 
