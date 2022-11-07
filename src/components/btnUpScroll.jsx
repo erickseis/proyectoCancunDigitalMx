429d16ffb73bb573351ab34e36be737333dc4bb2
erickseis <erickseis@gmail.com>
2022-11-06 21:37:09 -0300

    carga

--- src/components/btnUpScroll.jsx
+++ src/components/btnUpScroll.jsx
@@ -6,8 +6,6 @@ export default function BtnUpScroll() {
   return (
     <div className='BtnUpScroll'>
       <ScrollToTop className='btn-smooth' smooth color='white' />
-      <h1>Scroll To See Magic</h1>
-      <p style={{ marginTop: '200vh' }}>You Have Reached Bottom</p>
     </div>
   );
 }
